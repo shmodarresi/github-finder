ef7d31f0ea1fc7dbfe23d1950fdf865a886c2faa
Armitis Dev <armitis.dev@outlook.com>
2020-01-25 23:28:43 +0100

    05-Create User Detail Page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react'
 
-function getTime(){
-    return (new Date()).toLocaleTimeString();
+const Navbar = ({ icon, title }) => {
+    // window.navigator.geolocation.getCurrentPosition(
+    //     (position) => console.log(position),
+    //     (err) => console.log(err)
+    // );
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() =>{
+        setInterval(() =>{
+            setTime(new Date());
+        },1000)
+    })
+
+
+    return (
+        <nav className="navbar bg-danger text-light">
+            <h2>
+                <i className={icon}></i> {title}
+
+            </h2>
+            <span className="float-right">{time.toLocaleTimeString()}</span>
+        </nav>
+    )
 }
 
-const Navbar = ({icon,title}) => {
-        window.navigator.geolocation.getCurrentPosition(
-            (position)=>console.log(position),
-            (err) => console.log(err)
-        );
-
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <h2>
-                    <i className={icon}></i> {title}
-                    
-                </h2>
-                <span className="float-right">{getTime()}</span>
-            </nav>
-        )
-}
-
-Navbar.defaultProps={
-    title:'Github Finder',
-    icon:'fa fa-github'
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fa fa-github'
 };
 
 export default Navbar
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getUser(this.props.match.params.username);
     }
 
     render() {
-        const {login , name, company, blog, bio, public_repos, public_gists, html_url} = this.props.user;
+        const { login, name, avatar_url, company, blog, bio, public_repos, public_gists, html_url, followers, following, hireable } = this.props.user;
 
-        if(this.props.loading){
+        if (this.props.loading) {
 
-            return <Spinner/>  
+            return <Spinner />
 
-       }else{
+        } else {
 
-        return (
-            <div>
-                {name}
-            </div>
-        );
-       }
+            return (
+                <div>
+                    <header className="d-flex flex-row my-1">
+                    <Link to="/" className="btn btn-light mr-auto">Back to search</Link> 
+                    <span className="d-inline-flex flex-row align-items-center">Hireable {hireable ? <i className="fa fa-check-circle text-success ml-1"></i> : <i className="fa fa-times-circle text-danger ml-1"></i>}</span>
+                    </header>
+                
+                <div className="card flex-row py-2">
+                    <div className="text-center col">
+                        <img src={avatar_url}  className="rounded-circle" style={{width:150}}/>
+                        <div>{name}</div>
+                    </div>
+                    <div className="col-8">
+                        <strong>Bio:</strong>
+                        <p>{bio}</p>
+                        <strong>Company: </strong>{company}<br/>
+                        <a className="btn btn-primary mt-3" href={html_url}>Link to profile</a>
+                    </div>
+                </div>
+                <div className="card flex-row justify-content-center mt-2 py-2">
+                    <ul className="m-0">
+                        <li className="badge badge-secondary mr-1">Followers: {followers}</li>
+                        <li className="badge badge-success mr-1">Following: {following}</li>
+                        <li className="badge badge-primary mr-1">Repos: {public_repos}</li>
+                        <li className="badge badge-danger">Gists: {public_gists}</li>
+                    </ul>
+                </div>
+                </div>
+            );
+        }
     }
 }
 
